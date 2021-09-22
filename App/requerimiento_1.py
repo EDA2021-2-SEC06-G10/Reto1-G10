fb70593052555b3b8a547c67a906f80aa434423a
LAPTOP-VH5HOLUM\andre <al.beltran@uniandes.edu.co>
2021-09-22 00:15:31 -0500

    aaa

--- App/requerimiento_1.py
+++ App/requerimiento_1.py
@@ -16,18 +16,18 @@ def requerimiento_1(catalog, anio_inicial, anio_final):
     primero = 0
     ultimos = 0
     start_time = time.process_time()
-    for cat in lt.iterator(catalog["BeginDate"]):
-        if anio_final >= cat["BeginDate"] >= anio_inicial:
+    for cat in lt.iterator(catalog["nacimiento"]):
+        if anio_final >= cat["nacimiento"] >= anio_inicial:
             artista = lt.getElement(artistas, cat)
             lt.insertElement(artistas_por_anio,artistas,cat)
             contador_de_artistas +=1
 
     for art in (1,lt.size(artistas_por_anio)):
-        if art["BeginDate"] == anio_inicial:
-            lt.addFirst(rta,art["DisplayName"]["BeginDate"]["EndDate"]["Nationality"]["Gender"])
+        if art["nacimiento"] == anio_inicial:
+            lt.addFirst(rta,art["nombre"]["nacimiento"]["fallecimiento"]["nacimiento"]["Genero"])
 
-        if art["BeginDate"] == anio_final:
-            lt.addLast(rta,art["DisplayName"]["BeginDate"]["EndDate"]["Nationality"]["Gender"])
+        if art["nacimiento"] == anio_final:
+            lt.addLast(rta,art["nombre"]["nacimiento"]["fallecimiento"]["nacimiento"]["Genero"])
     
 
     x= ("hay ", contador_de_artistas , " artistas nacidos entre", anio_inicial, "y", anio_final, "adicional a esto los primeros y ultimos 3 artistas del rango son:",rta  )
